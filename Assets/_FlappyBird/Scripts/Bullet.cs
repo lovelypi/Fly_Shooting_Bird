421bf382d0d07e64e33b4b4c6ddf6748f6766ef4
lovelypi <minhtuanmvp@gmail.com>
2024-01-11 23:04:31 +0700

    Shooting + Add Tools

---
+++ Assets/_FlappyBird/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+using System;
+using GameTool;
+using UnityEngine;
+
+public class Bullet : BasePooling
+{
+    public float speed;
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+
+    private void OnEnable()
+    {
+        rb.velocity = new Vector2(speed, 0);
+    }
+}
--- Assets/_FlappyBird/Scripts/Player.cs
+++ Assets/_FlappyBird/Scripts/Player.cs
@@ -1,14 +1,17 @@
-using System;
+using GameTool;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float jumpForce = 20f;
+    public float cooldown;
+    public float timeShoot = 2.0f;
     
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cooldown = timeShoot;
     }
 
     private void Update()
@@ -16,6 +19,13 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             rb.velocity = new Vector2(0, jumpForce);
+        }
+
+        cooldown -= Time.deltaTime;
+        if (cooldown <= 0.0f)
+        {
+            PoolingManager.Instance.GetObject(NamePrefabPool.Bullet, null, transform.position).Disable(1);
+            cooldown = timeShoot;
         }
     }
 
